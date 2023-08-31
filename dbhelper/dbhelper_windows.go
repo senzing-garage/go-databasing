fd913cdb7a7278fa7c7810f0a8bfe04192441dc3
docktermj <michael@dockter.com>
2023-08-31 17:04:12 -0400

    #54 Added ExtractSqliteDatabaseFilename

--- dbhelper/dbhelper_windows.go
+++ dbhelper/dbhelper_windows.go
@@ -10,7 +10,7 @@ import (
 
 func extractSqliteDatabaseFilenameForOsArch(parsedDatabaseUrl *url.URL) (string, error) {
 	result := parsedDatabaseUrl.Path
-	if !strings.HasPrefix(result, "/") {
+	if strings.HasPrefix(result, "/") {
 		fmt.Printf(">>>>>>>>>>>>>> before: %s", result)
 		result = result[2:]
 		fmt.Printf(">>>>>>>>>>>>>>  after: %s", result)
