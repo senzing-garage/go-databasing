468d7b924018a5f847c2063f29bc187b2ea197c7
docktermj <michael@dockter.com>
2025-03-07 09:13:00 -0500

    #133 Testing

--- connector/connector_test.go
+++ connector/connector_test.go
@@ -7,62 +7,116 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type testCaseMetadata struct {
+	databaseURL      string
+	isBadDatabaseURL bool
+	name             string
+}
+
+var testCases = []testCaseMetadata{
+	{
+		name:        "oci-001",
+		databaseURL: "oci://username:password@hostname:1521/G2",
+	},
+	{
+		name:        "oci-002",
+		databaseURL: "oci://sys:Passw0rd@localhost:1521/FREE/?sysdba=true&noTimezoneCheck=true",
+	},
+	{
+		name:             "oci-003",
+		databaseURL:      "oci://username:password@G2",
+		isBadDatabaseURL: true,
+	},
+	{
+		name:        "mssql-001",
+		databaseURL: "mssql://username:password@hostname:1433/G2",
+	},
+	{
+		name:        "mssql-002",
+		databaseURL: "mssql://username:password@hostname:1433/G2/?driver=mssqldriver",
+	},
+	{
+		name:        "mssql-003",
+		databaseURL: "mysql://username:password@hostname:3306/?schema=schemaname",
+	},
+	{
+		name:        "mssql-004",
+		databaseURL: "mssql://username:password@hostname:3306/database?schema=schemaname",
+	},
+	{
+		name:             "mssql-005",
+		databaseURL:      "mssql://username:password@hostname:1433:G2/?driver=mssqldriver",
+		isBadDatabaseURL: true,
+	},
+	{
+		name:             "mssql-006",
+		databaseURL:      "mssql://username:password@G2",
+		isBadDatabaseURL: true,
+	},
+	{
+		name:        "mysql-001",
+		databaseURL: "mysql://username:password@hostname:3306/G2",
+	},
+	{
+		name:        "mysql-002",
+		databaseURL: "mysql://username:password@hostname:3306/database?schema=schemaname",
+	},
+	{
+		name:             "mysql-003",
+		databaseURL:      "mysql://username:password@hostname:3306/?schema=G2",
+		isBadDatabaseURL: true,
+	},
+	{
+		name:        "postgresql-001",
+		databaseURL: "postgresql://username:password@hostname:5432/G2",
+	},
+	{
+		name:        "postgresql-002",
+		databaseURL: "postgresql://username:password@hostname:5432/G2/?schema=schemaname",
+	},
+	{
+		name:        "postgresql-003",
+		databaseURL: "postgresql://username:password@hostname:5432:G2/?schema=schemaname",
+	},
+	{
+		name:        "postgresql-004",
+		databaseURL: "postgresql://username:password@hostname:5432/database/?schema=schemaname",
+	},
+	{
+		name:             "postgresql-005",
+		databaseURL:      "postgresql://username:password@hostname:5432:database/?schema=schemaname",
+		isBadDatabaseURL: true,
+	},
+	{
+		name:             "postgresql-006",
+		databaseURL:      "postgresql://username:password@hostname:5432:G2/",
+		isBadDatabaseURL: true,
+	},
+	{
+		name:        "sqlite-001",
+		databaseURL: "sqlite3://na:na@/tmp/sqlite/G2C.db",
+	},
+	{
+		name:             "bad-protocol-001",
+		databaseURL:      "badProtocol://username:password@hostname:3306/database?schema=schemaname",
+		isBadDatabaseURL: true,
+	},
+}
+
 // ----------------------------------------------------------------------------
 // Test interface functions
 // ----------------------------------------------------------------------------
 
-func TestNewConnector_badURLProtocol(test *testing.T) {
+func TestNewConnector(test *testing.T) {
 	ctx := context.TODO()
-	databaseURL := "badProtocol://username:password@hostname:3306/database?schema=schemaname"
-	_, err := NewConnector(ctx, databaseURL)
-	require.Error(test, err)
-}
-
-func TestNewConnector_mysql1(test *testing.T) {
-	ctx := context.TODO()
-	databaseURL := "mysql://username:password@hostname:3306/database?schema=schemaname"
-	_, err := NewConnector(ctx, databaseURL)
-	require.NoError(test, err)
-}
-
-func TestNewConnector_mysql2(test *testing.T) {
-	ctx := context.TODO()
-	databaseURL := "mysql://username:password@hostname:3306/?schema=schemaname"
-	_, err := NewConnector(ctx, databaseURL)
-	require.NoError(test, err)
-}
-
-func TestNewConnector_mssql1(test *testing.T) {
-	ctx := context.TODO()
-	databaseURL := "mssql://username:password@hostname:3306/database?schema=schemaname"
-	_, err := NewConnector(ctx, databaseURL)
-	require.NoError(test, err)
-}
-
-func TestNewConnector_oracle1(test *testing.T) {
-	ctx := context.TODO()
-	databaseURL := "oci://sys:Passw0rd@localhost:1521/FREE/?sysdba=true&noTimezoneCheck=true"
-	_, err := NewConnector(ctx, databaseURL)
-	require.NoError(test, err)
-}
-
-func TestNewConnector_postgresql1(test *testing.T) {
-	ctx := context.TODO()
-	databaseURL := "postgresql://username:password@hostname:5432:database/?schema=schemaname"
-	_, err := NewConnector(ctx, databaseURL)
-	require.NoError(test, err)
-}
-
-func TestNewConnector_postgresql2(test *testing.T) {
-	ctx := context.TODO()
-	databaseURL := "postgresql://username:password@hostname:5432/database/?schema=schemaname"
-	_, err := NewConnector(ctx, databaseURL)
-	require.NoError(test, err)
-}
-
-func TestNewConnector_sqlite(test *testing.T) {
-	ctx := context.TODO()
-	databaseURL := "sqlite3://na:na@/tmp/sqlite/G2C.db"
-	_, err := NewConnector(ctx, databaseURL)
-	require.NoError(test, err)
+	for _, testCase := range testCases {
+		test.Run(testCase.name, func(test *testing.T) {
+			_, err := NewConnector(ctx, testCase.databaseURL)
+			if testCase.isBadDatabaseURL {
+				require.Error(test, err)
+			} else {
+				require.NoError(test, err)
+			}
+		})
+	}
 }
