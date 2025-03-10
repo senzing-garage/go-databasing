a39ee4693e3c83d3be2f5a671d7d18e8e9e2c972
docktermj <michael@dockter.com>
2025-03-10 11:14:40 -0400

    #133 Working make test

--- connector/connector_test.go
+++ connector/connector_test.go
@@ -15,18 +15,6 @@ type testCaseMetadata struct {
 
 var testCases = []testCaseMetadata{
 	{
-		name:        "oci-001",
-		databaseURL: "oci://username:password@hostname:1521/G2",
-	},
-	{
-		name:        "oci-002",
-		databaseURL: "oci://sys:Passw0rd@localhost:1521/FREE/?sysdba=true&noTimezoneCheck=true",
-	},
-	{
-		name:        "oci-003",
-		databaseURL: "oci://username:password@G2",
-	},
-	{
 		name:        "mssql-001",
 		databaseURL: "mssql://username:password@hostname:1433/G2",
 	},
@@ -62,6 +50,18 @@ var testCases = []testCaseMetadata{
 	{
 		name:        "mysql-003",
 		databaseURL: "mysql://username:password@hostname:3306/?schema=G2",
+	},
+	{
+		name:        "oci-001",
+		databaseURL: "oci://username:password@hostname:1521/G2",
+	},
+	{
+		name:        "oci-002",
+		databaseURL: "oci://sys:Passw0rd@localhost:1521/FREE/?sysdba=true&noTimezoneCheck=true",
+	},
+	{
+		name:        "oci-003",
+		databaseURL: "oci://username:password@G2",
 	},
 	{
 		name:        "postgresql-001",
@@ -101,6 +101,38 @@ var testCases = []testCaseMetadata{
 	},
 }
 
+type testCaseConnect struct {
+	databaseURL string
+	name        string
+}
+
+var testCasesConnect = []testCaseConnect{
+	{
+		name:        "mssql-001",
+		databaseURL: "mssql://sa:Passw0rd@localhost/master",
+	},
+	{
+		name:        "mysql-001",
+		databaseURL: "mysql://root:root@localhost/G2",
+	},
+	{
+		name:        "oci-001",
+		databaseURL: "oci://sys:Passw0rd@localhost:1521/FREE/?sysdba=true&noTimezoneCheck=true",
+	},
+	{
+		name:        "postgresql-001",
+		databaseURL: "postgresql://postgres:postgres@localhost/G2/?sslmode=disable",
+	},
+	{
+		name:        "sqlite-001",
+		databaseURL: sqliteDatabaseURL, // Platform dependent URL
+	},
+	{
+		name:        "sqlite-002",
+		databaseURL: sqliteDatabaseURL + "?mode=memory&cache=shared", // Platform dependent URL
+	},
+}
+
 // ----------------------------------------------------------------------------
 // Test interface functions
 // ----------------------------------------------------------------------------
@@ -119,20 +151,14 @@ func TestNewConnector(test *testing.T) {
 	}
 }
 
-// func TestNewConnector_connect(test *testing.T) {
-// 	ctx := context.TODO()
-// 	for _, testCase := range testCases {
-// 		test.Run(testCase.name, func(test *testing.T) {
-// 			databaseConnector, err := NewConnector(ctx, testCase.databaseURL)
-// 			if testCase.isBadDatabaseURL {
-// 				require.Error(test, err)
-// 			} else {
-// 				require.NoError(test, err)
-// 				if !testCase.doNotTestConnect {
-// 					_, err = databaseConnector.Connect(ctx)
-// 					require.NoError(test, err)
-// 				}
-// 			}
-// 		})
-// 	}
-// }
+func TestNewConnector_connect(test *testing.T) {
+	ctx := context.TODO()
+	for _, testCase := range testCasesConnect {
+		test.Run(testCase.name, func(test *testing.T) {
+			databaseConnector, err := NewConnector(ctx, testCase.databaseURL)
+			require.NoError(test, err)
+			_, err = databaseConnector.Connect(ctx)
+			require.NoError(test, err)
+		})
+	}
+}
