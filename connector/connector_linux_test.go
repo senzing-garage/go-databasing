7dc4e2f366bae10a3e4f3fe0f3e991315971f539
docktermj <michael@dockter.com>
2025-03-10 11:19:23 -0400

    #133 Working make test

---
+++ connector/connector_linux_test.go
@@ -0,0 +1,5 @@
+//go:build linux
+
+package connector
+
+var sqliteDatabaseURL = "sqlite3://na:na@/tmp/sqlite/G2C.db"
---
+++ connector/connector_windows_test.go
@@ -0,0 +1,5 @@
+//go:build windows
+
+package connector
+
+var sqliteDatabaseURL = `sqlite3://na:na@nowhere/C:\Temp\sqlite\G2C.db`
